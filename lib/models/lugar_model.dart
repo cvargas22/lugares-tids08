dce03dc70bdda5fba99e24897ebc144457beffbd
cvargas22 <cesar-ed22@hotmail.com>
2020-11-11 00:01:53 -0300

    se crea modelo para lugares y db helper para las transacciones

---
+++ lib/models/lugar_model.dart
@@ -0,0 +1,33 @@
+
+
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+
+class Ubicacion{
+  final double latitud;
+  final double longitud;
+  final String direccion;
+
+  Ubicacion({
+    @required this.latitud,
+    @required this.longitud,
+    this.direccion
+  });
+}
+
+class Lugar{
+  final String id;
+  final String titulo;
+  final Ubicacion ubicacion;
+  final File imagen;
+
+  Lugar({
+    @required this.id,
+    @required this.titulo,
+    @required this.ubicacion,
+    @required this.imagen
+  });
+}
